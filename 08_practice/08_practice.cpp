0c2196491c7adcd2ce2af99bbc0c12bbbd81ae7b
thugan <thuganenko@gmail.com>
2026-09-14 20:37:54 +0300

    08

---
+++ 08_practice/08_practice.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+using namespace std;
+void sqere(int arr[][10], int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+int factorial(int fact) 
+{
+    int sum = 1;
+    for (int i = 1; i <= fact; i++)
+    {
+        sum *= i;
+    }
+    return sum;
+}
+void simple(int num)
+{
+    int cicle = 1;
+    for (int i = 2; i < num; i++) {
+        if (num % i == 0) {
+            cicle +=1;
+            break;
+        }
+    }
+    if (cicle == 1) {
+        cout << "SIMPLE" << endl;
+    }
+    else {
+        cout << "NO SIMPLE" << endl;
+    }
+}
+int max1, index,min;
+void maxNum(int arr[],int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        if (arr[i] > ::max1)
+        {
+            ::max1 = arr[i];
+            index = i;
+        }
+    }
+    cout << "MAx element in the array " << ::max1 <<" on "<<::index<<" index!"<< endl;
+}
+void minNum(int arr[],int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        if (arr[i] < ::min)
+        {
+            ::min = arr[i];
+            index = i;
+        }
+    }
+    cout << "Min element in the array " << ::min <<" on "<<::index<<" index!"<< endl;
+}
+int cube(int num)
+{
+    int sum = num;
+    for (int i = 1; i < 3; i++)
+    {
+        sum *= num;
+    }
+    return sum;
+}
+int isPlOrMn(int num) 
+{
+    if (num < 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+int main()
+{
+    const int column = 10;
+    const int row = 10;
+    int arr[row][column] = {};
+    sqere(arr, row, column);
+
+    int fact = 5;
+    cout << "Factorial : " << factorial(fact)<<endl;
+
+    int smpl = 5;
+    simple(smpl);
+
+    const int len1 = 6;
+    int arr1[len1] = {1,7,3,4,-8,6};
+    maxNum(arr1,len1);
+    minNum(arr1,len1);
+
+    int num1 = 5;
+    cout << cube(num1) << endl;
+
+    int num2 = 1;
+    int num3 = -1;
+    cout << isPlOrMn(num2) <<" "<<isPlOrMn(num3) << endl;
+}
---
+++ function/function.cpp
@@ -0,0 +1,218 @@
+﻿#include <iostream>
+using namespace std;
+
+void Hello();//prototype functions
+void sayHello()
+{
+    cout << "Hello world" << endl;
+}
+
+void Star(int count) //1500
+{
+    for (int i = 0; i < count; i++)
+    {
+        cout << "* ";
+    }
+    cout << endl;
+}
+void AnyLine(char symbol, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        cout << symbol << " ";
+    }
+    cout << endl;
+}
+
+int MyPow(int number, int step)// 2**5 2*2*2*2*2
+{
+    int pow = 1;
+    for (int i = 0; i < step; i++)
+    {
+        pow *= number;
+    }
+    //cout << "Pow number " << number << " is " << pow << endl;
+    return pow;
+}
+int Max(int a, int b) {
+    /* if (a > b)
+     {
+         return a;
+     }
+     else
+     {
+         return b;
+     }*/
+    return (a > b) ? a : b;
+}
+int Min(int a, int b) {
+    return (a < b) ? a : b;
+}
+void Second()
+{
+    cout << "Second function working...." << endl;
+}
+void First()
+{
+
+    cout << "First function start" << endl;
+    Second();
+    cout << "First function end" << endl;
+
+}
+void Change(int a, int b)
+{
+    cout << "a = " << a << " . b = " << b << endl;
+    int temp = a;
+    a = b;
+    b = temp;
+    cout << "a = " << a << " . b = " << b << endl;
+}
+void InitArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        //900%90= 0.....89   + 10
+        //rand() % 90  --> 
+        arr[i] = rand() % 90 + 10;//
+    }
+}
+void ShowArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int SummaArray(int arr[], int size)
+{
+    int summa = 0;
+    for (int i = 0; i < size; i++)
+    {
+        summa += arr[i];
+    }
+    return summa;
+}
+void InitMatrix(int arr[][7], int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            arr[i][j] = rand() % 90 + 10;
+        }
+    }
+}
+void ShowMatrix(int arr[][7], int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int A = 100;//gloabal variable 
+
+void setA()
+{
+    A = 99;
+}
+void Show()
+{
+    cout << A << endl;
+}
+void setSecond()
+{
+    int A = 77;
+    cout << A << endl;
+}
+int main()
+{
+    Show();
+    setA();
+    Show();
+    setSecond();
+    Show();
+    //int A = 10;//local variable
+    cout << A << endl;
+    cout << ::A << endl;
+    ::A = 33;
+    cout << ::A << endl;
+
+
+    //int a = 5;
+    //if (a == 5)
+    //{
+    //    int b = 7;
+    //    cout << b << endl;
+    //}
+    //cout << b << endl;
+
+
+
+
+
+
+    const int rows = 5;
+    const int cols = 7;
+    int array[rows][cols];
+    InitMatrix(array, rows, cols);
+    ShowMatrix(array, rows, cols);
+
+
+
+
+
+    const int size = 10;
+    int arr[size];
+    InitArray(arr, size);
+    ShowArray(arr, size);
+    int summa = SummaArray(arr, size);
+    cout << "Summa elements = " << summa << endl;
+
+
+    int a = 4, b = 7;
+    cout << "a = " << a << " . b = " << b << endl;
+    Change(a, b);
+    cout << "a = " << a << " . b = " << b << endl;
+    First();
+
+
+
+
+
+    cout << "Min element = " << Min(5, 8) << endl;
+    cout << "Min element = " << Min(15, 4) << endl;
+    cout << "Max element = " << Max(5, 8) << endl;
+    cout << "Max element = " << Max(15, 4) << endl;
+    cout << "Res = " << MyPow(2, 2) << endl;
+    int res = MyPow(2, 3);
+    cout << "Res = " << res << endl;
+    MyPow(2, 5);
+    AnyLine('$', 65);
+    AnyLine('*', 15);
+    AnyLine('@', 74);
+    Star(10);
+    Star(15);
+    Star(25);
+    //Функція оголошується до функції main.
+    sayHello();
+    sayHello();
+    sayHello();
+    sayHello();
+    //Функція оголошується за допомогою прототипу
+    Hello();
+
+
+
+}
+
+void Hello()
+{
+    cout << "Hello world" << endl;
+}
